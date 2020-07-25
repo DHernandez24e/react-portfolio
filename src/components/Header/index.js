e76a67778ec378755af8661939df6845bfb7d4ee
Doggo <dhernandez24e@hotmail.com>
2020-07-25 16:09:57 -0700

    add inital components

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../Navigation';
+
+function Header() {
+
+    return(
+        <header>
+            This is a header
+            <Nav></Nav>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Nav() {
+
+
+    return(
+        <nav>
+            <ul>
+                <li>Item One</li>
+                <li>Item Two</li>
+                <li>Item Three</li>
+                <li>Item Four</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
