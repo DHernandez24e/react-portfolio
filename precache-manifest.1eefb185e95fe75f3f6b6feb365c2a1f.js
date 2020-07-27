b610d5a22f346ce8f75e5a7fd2b4bdb799f3d485
Doggo <dhernandez24e@hotmail.com>
2020-07-26 22:25:51 -0700

    Updates

---
+++ precache-manifest.1eefb185e95fe75f3f6b6feb365c2a1f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138098a713298db207385a673ee8c325",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "1532fd456b1bc96a205e",
+    "url": "/react-portfolio/static/css/main.be782a69.chunk.css"
+  },
+  {
+    "revision": "f08e03368eedd8ce3a89",
+    "url": "/react-portfolio/static/js/2.cc604052.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.cc604052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1532fd456b1bc96a205e",
+    "url": "/react-portfolio/static/js/main.94fed45b.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "511df7cb8ca821551a3323ba0dc6b844",
+    "url": "/react-portfolio/static/media/1.511df7cb.jpg"
+  },
+  {
+    "revision": "cdf222e772e48cd7be7016ad8dac8736",
+    "url": "/react-portfolio/static/media/2.cdf222e7.jpg"
+  },
+  {
+    "revision": "6908204b409de34453dd1a7c1de277e3",
+    "url": "/react-portfolio/static/media/3.6908204b.jpg"
+  },
+  {
+    "revision": "c49693e4b1f9eb5f514e5071445d0d96",
+    "url": "/react-portfolio/static/media/4.c49693e4.jpg"
+  },
+  {
+    "revision": "94cef5094defd985e7b139243912d7fb",
+    "url": "/react-portfolio/static/media/5.94cef509.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
